4d07b29ac02ce9623c777494e6c0769481e931fc
longfei.liu <longfei.liu@thoughtworks.com>
2019-09-06 17:00:55 +0800

    [Liu Longfei] feat: finish InverseReduce exercise

--- src/main/java/com/thoughtworks/collection/InverseReduce.java
+++ src/main/java/com/thoughtworks/collection/InverseReduce.java
@@ -2,6 +2,7 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -16,6 +17,12 @@ public class InverseReduce {
     }
 
     public List<Integer> divideToSmaller(int number) {
-        throw new NotImplementedException();
+        List<Integer> list = new ArrayList<>();
+        int n = random.nextInt(3);
+        while (number > n) {
+            list.add(number - n);
+            number -= 2;
+        }
+        return list;
     }
 }
